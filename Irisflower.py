2b43a5a599e063937fe977cefeebfe46cbc063ca
AnotherHari <anotherhari06@gmail.com>
2025-08-28 18:55:13 +0530

    Minor updates

--- Irisflower.py
+++ Irisflower.py
@@ -20,10 +20,8 @@ X= iris.data
 y= iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
-
 model = LogisticRegression(max_iter=200)
 model.fit(X_train,y_train)
-
 y_pred = model.predict(X_test)
 
 print("\nAccuracy:", accuracy_score(y_test, y_pred))
